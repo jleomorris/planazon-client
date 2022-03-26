264abfd1924e90e639dae848f136c491b6b89350
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-26 14:47:35 +0000

    Wrap app in ApolloProvider

--- start/client/src/App.js
+++ start/client/src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CategoryPage from "./pages/CategoryPage";
 import AnimalPage from "./pages/AnimalPage/AnimalPage";
-import { ApolloClient, InMemoryCache, inMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -15,14 +15,21 @@ const client = new ApolloClient({
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Route exact strict path="/" component={LandingPage} />
-          <Route exact strict path="/products/:slug" component={CategoryPage} />
-          <Route exact strict path="/product/:slug" component={AnimalPage} />
-        </Switch>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Nav />
+          <Switch>
+            <Route exact strict path="/" component={LandingPage} />
+            <Route
+              exact
+              strict
+              path="/products/:slug"
+              component={CategoryPage}
+            />
+            <Route exact strict path="/product/:slug" component={AnimalPage} />
+          </Switch>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
