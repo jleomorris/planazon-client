1f4c24c2044ea90407b03365e560fb4596e95c1e
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-26 15:56:42 +0000

    Add mainCards fetch and render logic to MainHero

--- start/client/src/components/MainHero/MainHero.js
+++ start/client/src/components/MainHero/MainHero.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import "./MainHero.css"
-import animals from "../../assets/images"
-import {Container} from "reactstrap"
+import React from "react";
+import "./MainHero.css";
+import animals from "../../assets/images";
+import { Container } from "reactstrap";
+import { useQuery, gql } from "@apollo/client";
 
 function MainHero() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      mainCards {
+        image
+        title
+      }
+    }
+  `);
 
-    return (
-        <div className="MainHero">
-            <Container>
-                    <div className="header-container">
-                        <h2>Find your <br/> new four-legged <br/> best friend</h2>
-                        <img src={animals.rhino} />
-                    </div>
-                    <div className="cards-container">
-                        {[].map(card => {
-                            return (
-                                <div className="card">
-                                    <h3>{card.title}</h3>
-                                    <img src={animals[card.img]} style={{width: "100%"}}/>
-                                </div>
-                            )
-                        })}
-                    </div>
-            </Container>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>An error occured</div>;
+
+  return (
+    <div className="MainHero">
+      <Container>
+        <div className="header-container">
+          <h2>
+            Find your <br /> new four-legged <br /> best friend
+          </h2>
+          <img src={animals.rhino} alt="main hero" />
         </div>
-    )
+        <div className="cards-container">
+          {data.mainCards.map((card) => {
+            return (
+              <div className="card" key={card.title}>
+                <h3>{card.title}</h3>
+                <img
+                  src={animals[card.image]}
+                  style={{ width: "100%" }}
+                  alt="animal main card"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default MainHero
+export default MainHero;
