fcd1b48cec02d22c3c640576585ca84ffaf837bd
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-26 16:54:38 +0000

    Add categories fetch logic to CategoryDisplay

--- start/client/src/components/CategoryDisplay/CategoryDisplay.jsx
+++ start/client/src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -1,29 +1,47 @@
-import React, { useState } from 'react'
-import "./CategoryDisplay.css"
-import { Container } from 'react-bootstrap'
-import animals from '../../assets/images'
+import React, { useState } from "react";
+import "./CategoryDisplay.css";
+import { Container } from "react-bootstrap";
+import animals from "../../assets/images";
+import { useQuery, gql } from "@apollo/client";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
+const CATEGORIES_QUERY = gql`
+  {
+    categories {
+      id
+      category
+      image
+      slug
+    }
+  }
+`;
 
 function CategoryDisplay() {
+  const { loading, error, data } = useQuery(CATEGORIES_QUERY);
 
-    return (
-        <div className="CategoryDisplay">
-            <Container className="CategoryDisplay-container">
-                {[].map(category => {
-                    return (
-                        <Link to={`/products/${category.slug}`} className="CategoryDisplay-card-container">
-                            <div className="CategoryDisplay-card">
-                                <img src={animals[category.img]} /> 
-                            </div>
-                            <h3>{category.category}</h3>
-                        </Link>
-                    )
-                })}
-            </Container>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>An error occured</div>;
+
+  return (
+    <div className="CategoryDisplay">
+      <Container className="CategoryDisplay-container">
+        {data.categories.map((category) => {
+          return (
+            <Link
+              to={`/products/${category.slug}`}
+              className="CategoryDisplay-card-container"
+            >
+              <div className="CategoryDisplay-card">
+                <img src={animals[category.image]} alt="category" />
+              </div>
+              <h3>{category.category}</h3>
+            </Link>
+          );
+        })}
+      </Container>
+    </div>
+  );
 }
 
-export default CategoryDisplay
+export default CategoryDisplay;
