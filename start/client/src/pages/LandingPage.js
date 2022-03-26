d5892de4c9d914c97840031e64ac513d09d1ff11
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-26 16:54:17 +0000

    Add animals fetch logic to LandingPage

--- start/client/src/pages/LandingPage.js
+++ start/client/src/pages/LandingPage.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import MainHero from "../components/MainHero/MainHero"
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
-import CardDisplay from "../components/CardDisplay/CardDisplay"
+import React from "react";
+import MainHero from "../components/MainHero/MainHero";
+import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay";
+import CardDisplay from "../components/CardDisplay/CardDisplay";
+import { useQuery, gql } from "@apollo/client";
+
+const ANIMALS_QUERY = gql`
+  {
+    animals {
+      image
+      title
+      id
+      price
+      slug
+      image
+    }
+  }
+`;
 
 function LandingPage() {
+  const { loading, error, data } = useQuery(ANIMALS_QUERY);
 
-    return (
-        <div>
-            <MainHero />
-            <CategoryDisplay />
-            <CardDisplay animals={[]}/>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>An error occured</div>;
+
+  return (
+    <div>
+      <MainHero />
+      <CategoryDisplay />
+      <CardDisplay animals={data.animals} />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
