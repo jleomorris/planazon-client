b66d8a5b5774c4779a7d77dc50cd317e987de105
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-03-27 14:11:44 +0100

    Add fetch and render logic to CategoryPage

--- start/client/src/pages/CategoryPage.js
+++ start/client/src/pages/CategoryPage.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import { useParams } from "react-router-dom"
-import { Container } from 'react-bootstrap'
-import CardDisplay from '../components/CardDisplay/CardDisplay'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import CardDisplay from "../components/CardDisplay/CardDisplay";
+import { useQuery, gql } from "@apollo/client";
+
+const CATEGORY_QUERY = gql`
+  query ($slug: String!) {
+    category(slug: $slug) {
+      id
+      category
+      slug
+      animals {
+        id
+        title
+        price
+        image
+      }
+    }
+  }
+`;
 
 function CategoryPage() {
-    
-    const { slug } = useParams()
+  const { slug } = useParams();
+  const { error, data, loading } = useQuery(CATEGORY_QUERY, {
+    variables: {
+      slug: slug,
+    },
+  });
 
-    return (
-        <div className="py-5">
-            <Container>
-                <h1 className="text-capitalize">
-                    {}
-                    <CardDisplay 
-                        animals={[]}
-                    />
-                </h1>
-            </Container>
-        </div>
-    )
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>An error occured</div>;
+
+  return (
+    <div className="py-5">
+      <Container>
+        <h1 className="text-capitalize">
+          {data.category.category}
+          <CardDisplay animals={data.category.animals} />
+        </h1>
+      </Container>
+    </div>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
