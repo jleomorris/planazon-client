6c3761dc2b3d299efc1ed54db917e8874aba5649
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-04-03 12:43:26 +0100

    Remove server files

--- api/resolvers/Category.js
+++
@@ -1,8 +0,0 @@
-const Category = {
-  products: (parent, args, { products }) => {
-    console.log(parent);
-    return products.filter((product) => product.category === parent.id);
-  },
-};
-
-module.exports = Category;
--- api/resolvers/Product.js
+++
@@ -1,9 +0,0 @@
-const Product = {
-  category: (parent, args, { categories }) => {
-    console.log(parent);
-    return categories.find((category) => category.id === parent.category);
-    // return animals.filter((animal) => animal.category === parent.id);
-  },
-};
-
-module.exports = Product;
